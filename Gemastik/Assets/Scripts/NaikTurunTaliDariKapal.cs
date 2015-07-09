6eae2786fb7df3fa5cb7d60e1a76caf50ee312b0
aghagha <amrikudou@gmail.com>
2015-07-09 22:24:48 +0700

    updated 'rope 2d' scene
    progres terakhir sebelum masuk 'gameplay' baru

---
+++ Gemastik/Assets/Scripts/NaikTurunTaliDariKapal.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NaikTurunTaliDariKapal : MonoBehaviour {
+	public GameObject camera;
+	private int waktuNaik = 0;
+	private int kedalaman = 0;
+	private bool jalaDilempar = false;
+	private bool lemparPertama = true;
+	private float kecepatanAwal = 4f;
+	private float kecepatanNaik = 0.5f;
+	private float kecepatanTurun = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+			Debug.Log ("kena");
+		}
+		if(jalaDilempar){
+			if (kedalaman < 50 && lemparPertama == true) {
+				transform.Translate (Vector2.down * kecepatanAwal * Time.deltaTime);
+				camera.transform.Translate (Vector2.down * kecepatanAwal * Time.deltaTime);
+				kedalaman++;
+			} else if(waktuNaik > 0){
+				transform.Translate (Vector2.up * kecepatanNaik * Time.deltaTime);
+				camera.transform.Translate (Vector2.up * kecepatanNaik * Time.deltaTime);
+				waktuNaik--;
+				kedalaman--;
+			} else {
+				lemparPertama = false;
+				transform.Translate (Vector2.down * kecepatanTurun * Time.deltaTime);
+				camera.transform.Translate (Vector2.down * kecepatanTurun * Time.deltaTime);
+				kedalaman++;
+			}
+		}
+	}
+
+	public void NaikPakaiTombol(){
+		waktuNaik = 30;
+	}
+
+	public void LemparJala(){
+		jalaDilempar = true;
+	}
+}
---
+++ Gemastik/Assets/Scripts/RotatePenggulungTali.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatePenggulungTali : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RotateLeft();
+	}
+
+	public void RotateRight(){
+		transform.Rotate(0, 0, -30, Space.World);
+	}
+
+	public void RotateLeft(){
+		transform.Rotate(0, 0, 40.0f * Time.deltaTime, Space.World);
+	}
+}
